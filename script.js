41a04e20345cd40458a750afcfd2195377737559
arungowdaedits <arungowda2926@gmail.com>
2025-07-24 18:34:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+function showSlides() {
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slideIndex = (slideIndex + 1) % slides.length;
+  slides[slideIndex].classList.add('active');
+}
+setInterval(showSlides, 3000);
