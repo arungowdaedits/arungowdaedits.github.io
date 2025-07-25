43462038021ec59c8034fe326605ea809b655d9d
arungowdaedits <arungowda2926@gmail.com>
2025-07-25 10:37:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let slideIndex = 0;
 const slides = document.querySelectorAll('.slide');
+
 function showSlides() {
   slides.forEach((slide) => slide.classList.remove('active'));
-  slideIndex = (slideIndex + 1) % slides.length;
-  slides[slideIndex].classList.add('active');
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].classList.add('active');
+  setTimeout(showSlides, 3000);
 }
-setInterval(showSlides, 3000);
+
+document.addEventListener('DOMContentLoaded', showSlides);
